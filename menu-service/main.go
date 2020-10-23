c50425b000ac805b939d3488d476b3693e333da9
syaripuddin <syaripuddin.digitalent>
2020-10-24 04:08:44 +0700

    commit

--- menu-service/main.go
+++ menu-service/main.go
@@ -1,18 +1,3 @@
-// package main
-
-// import (
-// 	"github.com/gorilla/mux"
-// )
-
-// func main(){
-// router := mux.NewRouter()
-
-// router.Handle("/add-menu", http.HandlerFunc(handler.AddMenu))
-
-// fmt.Handle("Menu service listen port : 8000")
-// log.Panic(htpp.ListenAndService)
-// }
-
 package main
 
 import (
